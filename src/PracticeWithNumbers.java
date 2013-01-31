4aab7bb2fed40bf423c644fb0e6b695140f34342
it1680505 <it1680505@ou134-206.ad.ilstu.edu>
2013-01-31 14:50:21 -0600

    First commit

---
+++ src/PracticeWithNumbers.java
@@ -0,0 +1,49 @@
+/*
+ * Modified on <1/22/2013>
+ * 
+ * ULID: <Rkhickm>
+ * Course: IT 168
+ * Lecture section: <4>
+ * Instructor: <Professor Matsuda>
+ * Lab section: <5>
+ * Instructor <Professor Erickson>
+ */
+
+/**
+ * Demonstrates that I understand how to declare variables, can label them correctly using printscreen, and can 
+ * use early arithmatic to solve problems. 
+ * 
+ * @author <Ryan Hickman>
+ *
+ */
+
+// My main public class
+public class PracticeWithNumbers {
+	// My main method
+	public static void main (String [] args){
+		
+		// Declares "answer1" as an int variable and uses the modulus arithmatic 
+		int answer1 = 5 % 3;
+		// Prints out on the screen "This is answer 1:" and then displays the variable "answer1" and its arithmatic
+		System.out.println("This is answer 1: " + answer1);
+		// Declares "answer2" as an int variable and uses the modulus arithmatic
+		int answer2 = 12 % 4;
+		// Prints out on the screen "This is answer 2:" and then displays the variable "answer2" and its arithmatic
+		System.out.println("This is answer 2: " + answer2);
+		// Declares "answer3" as an int variable and uses the modulus arithmatic
+		int answer3 = 3 % 7;
+		// Prints out on the screen "This is answer 3:" and then displays the variable "answer3" and its arithmatic
+		System.out.println("This is answer 3: " + answer3);
+		// Declares "answer4" as a double variable and uses the division arithmatic
+		double answer4 = 1/2.0;
+		// Prints out on the screen "This is answer 4:" and then displays the variable "answer4" and its arithmatic
+		System.out.println("This is answer 4: " + answer4);
+		// Declares "answer5" as a double variable and uses the division arithmatic
+		double answer5 = 3.0/2;
+		// Prints out on the screen "This is answer 5:" and then displays the variable "answer5" and its arithmatic
+		System.out.println("This is answer 5: " + answer5);
+		
+		
+	}
+
+}
